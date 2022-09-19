9633b42efa2841f8b43b392e7ab093994afe262f
PeterAlfredLee <peteralfredlee@gmail.com>
2022-09-19 19:18:48 +0800

    do not use comma-dangle

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,34 +3,34 @@ module.exports = {
     'browser': true,
     'es6': true,
     'node': true,
-    'jest': true,
+    'jest': true
   },
   'parser': '@babel/eslint-parser',
   'extends': 'eslint:recommended',
   'globals': {
     'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
+    'SharedArrayBuffer': 'readonly'
   },
   'parserOptions': {
     'ecmaVersion': 6,
     'sourceType': 'module',
     'ecmaFeatures': {
-      'impliedStrict': true,
+      'impliedStrict': true
     },
-    'requireConfigFile': false,
+    'requireConfigFile': false
   },
   'rules': {
-    'quotes': ['warn', 'single',],
-    'linebreak-style': [0, 'error', 'windows',],
-    'indent': ['error', 2,],
-    'semi': ['error', 'always',],
-    'comma-dangle': ['error', 'always',],
-    'no-cond-assign': ['error', 'always',],
+    'quotes': ['warn', 'single'],
+    'linebreak-style': [0, 'error', 'windows'],
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['warn', 'never'],
+    'no-cond-assign': ['error', 'always'],
     'no-console': 'off',
     'accessor-pairs': 'error',
     'default-case':'error',
     'no-eval': 'error',
     'no-use-before-define': 'warn',
-    'max-len': 'off',
-  },
+    'max-len': 'off'
+  }
 };
